5e18c58cb4d0682bdac83c707912d2a2228da95c
lishuang <lishuang_i@didichuxing.com>
2018-08-15 18:38:41 +0800

    模拟栈和队列

---
+++ stack/index.js
@@ -0,0 +1,15 @@
+//stack
+var arr = new Array()
+var count = arr.push('white', 'red', 'yellow')
+console.log(count)
+var item = arr.pop()
+console.log(item)
+console.log(arr.length)
+
+//queue
+var arr = new Array()
+var count = arr.push('white', 'red', 'yellow')
+console.log(count)
+var item = arr.shift()
+console.log(item)
+console.log(arr.length)
