8340a5a4d7766cf1993ed2d3b02e1d2af746305e
yangyunhan <1205385190@qq.com>
2018-12-06 23:47:43 +0800

    增加一种用0填位正序相加方法和一种用操作符简化的方法

--- addTwoBigNum/index.js
+++ addTwoBigNum/index.js
@@ -1,13 +1,52 @@
+/**
+ * 
+ * @param {Number} a 大整数
+ * @param {Number} b 大整数
+ */
 function add(a, b) {
-    var aLen = a.toString()
-    var bLen = b.toString()
-    var carry = 0, result = [], len = Math.max(aLen.length, bLen.length), i = len
+    var aStr = a.toString(), bStr = b.toString(), aLen = aStr.length, bLen = bStr.length;
+    var carry = 0, result = [], len = Math.max(aLen, bLen), i = len;
     while (i--) {
-        var sum = (+aLen[i - len + aLen.length] | 0) + (+bLen[i - len + bLen.length] | 0) + carry
-        var chu = sum / 10
-        carry = parseInt('' + chu)
-        result.unshift(sum % 10)
+        var sum = (+aStr[i - len + aLen] | 0) + (+bStr[i - len + bLen] | 0) + carry;
+        carry = Math.floor(sum / 10);
+        result.unshift(sum % 10);
     }
-    if(carry) result.unshift(carry)
-    return result.join('')
+    if (carry) result.unshift(carry);
+    return result.join('');
+}
+
+/**
+ * 
+ * @param {String} a 大整数的字符串表示
+ * @param {String} b 大整数的字符串表示
+ */
+function sumStrings(a, b) {
+    var result = [], count = 0;
+    //if (a.length < b.length) b = [a, a = b][0];
+    if (a.length < b.length) [a, b] = [b, a];
+    b = Array(a.length - b.length + 1).join('0') + b;
+    var arrA = a.split(''), arrB = b.split('');
+    for(var j = 0; j < a.length; j++){
+        var temp = (Number(arrA.pop()) + Number(arrB.pop())) + count;
+        temp > 10 ? [temp, count] = [temp - 10, 1] : count = 0;
+        result.unshift(temp);
+    }
+    result.unshift(count);
+    return result.join('').replace(/^0+/, '');
+}
+
+/**
+ * 
+ * @param {String} a 大整数的字符串表示 
+ * @param {String} b 大整数的字符串表示
+ */
+function sumStrings(a, b){
+    var res = '', c = 0;
+    a = a.split(''), b = b.split('');
+    while(a.length || b.length || c){
+        c += ~~a.pop() + ~~b.pop();
+        res = c % 10 + res;
+        c = c > 9;
+    }
+    return res.replace(/^0+/, '');
 }
